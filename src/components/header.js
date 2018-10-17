eed39941da0d21ce4276d30e246de2c3546377e2
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2018-10-17 16:32:13 +0700

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Nav from './nav';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <Nav />
+                <div className="head">
+                    <h1>We Creater,<br/> We Developer</h1>
+                    <div>
+                        <p>Deserunt incididunt ipsum eu cupidatat labore culpa voluptate elit voluptate velit elit dolor laborum amet. Qui sit magna incididunt ex sunt sit deserunt est est ex veniam proident. Et duis duis deserunt esse nulla amet nostrud adipisicing enim cupidatat. </p>
+                        <div><a className="contact" href="">Contact Me</a></div>
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
